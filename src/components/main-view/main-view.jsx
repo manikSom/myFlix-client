c6989432b31fc5bcb8879bf4d52400a2b64113c5
Manickam Somasundaram <manickam.som@gmail.com>
2023-02-27 16:53:57 +0000

    feat: connection api
    - the list of movies is taken from the myFlix api through heroku
    - data is rendered through components using PropTypes

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,5 @@
 // Importing useState for creation and initialization of state
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 // Importing MovieCard component, so it can be used here
 import { MovieCard } from '../movie-card/movie-card';
@@ -8,75 +8,82 @@ import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: 'Mayaanadhi',
-      description:
-        'The Protagonists troubled love and struggle for survival form the basic plot of the film',
-      image: 'https://upload.wikimedia.org/wikipedia/en/e/e6/Mayaanadhi_film_poster.jpg',
-      director: 'Aashiq Abu',
-      genre: 'Romance',
-    },
-    {
-      id: 2,
-      title: 'Angamaly Diaries',
-      description:
-        'The story follows Vincent Pepe who wants to be a powerful leader of a righteous gang that will rule the town of Angamaly.',
-      image: 'https://upload.wikimedia.org/wikipedia/en/4/4a/Angamaly_Diaries_Official_Poster.jpg',
-      director: 'Lijo Jose Pellissery',
-      genre: 'Drama',
-    },
-    {
-      id: 3,
-      title: 'Big B',
-      description:
-        'Big B is considered as a pathbreaking film in the history of Malayalam cinema, which introduced a new style of making and has later attained a huge cult following.',
-      image: 'https://upload.wikimedia.org/wikipedia/en/4/41/Big_B_film_poster.jpg',
-      director: 'Amal Neerad',
-      genre: 'Neo-Noir',
-    },
-    {
-      id: 4,
-      title: 'Comrade in America',
-      description:
-        'The film revolves around the travel of Aji, a communist from Kerala who travels to the United States illegally via Latin America to get to his girlfriend Sarah in time before she is forced to marry another person.',
-      image: 'https://upload.wikimedia.org/wikipedia/en/0/00/Comrade_in_America.jpg',
-      director: 'Amal Neerad',
-      genre: 'Romance',
-    },
-    {
-      id: 5,
-      title: 'Jallikattu ',
-      description:
-        'The plot follows a bull that escapes from a slaughterhouse in a hilly remote village and the entire village men gathering to hunt down the animal.',
-      image: 'https://upload.wikimedia.org/wikipedia/en/3/38/JallikkattuFirstLook.jpg',
-      director: 'Lijo Jose Pellissery',
-      genre: 'Thriller',
-    },
-  ]);
+    const [movies, setMovies] = useState([]);
+  
+    useEffect(() => {
+      fetch('https://mokkamovie.herokuapp.com/movies')
+        .then((response) => response.json())
+        .then((data) => {
+          const moviesFromApi = data.map((doc) => {
+            return {
+              id: doc._id,
+              title: doc.Title,
+              description: doc.Description,
+              genre: {
+                name: doc.Genre.Name,
+                description: doc.Genre.Description,
+              },
+              director: {
+                name: doc.Director.Name,
+                bio: doc.Director.Bio,
+                birth: doc.Director.Birth,
+                death: doc.Director.Death,
+              },
+              image: doc.ImagePath,
+            };
+          });
+          setMovies(moviesFromApi);
+          console.log('movies from api:', data);
+        });
+    }, []);
 
-  const [selectedMovie, setSelectedMovie] = useState(null);
+    const [selectedMovie, setSelectedMovie] = useState(null);
 
-  if (selectedMovie) {
-    return <MovieView movieData={selectedMovie} onBackClick={() => setSelectedMovie(null)} />;
-  }
+    if (selectedMovie) {
+        let similarMovies = movies.filter((movie) => {
+        return (
+            movie.genre.name === selectedMovie.genre.name &&
+            movie.title !== selectedMovie.title
+        );
+        });
+        console.log(similarMovies);
+        return (
+        <>
+            <MovieView
+            movieData={selectedMovie}
+            onBackClick={() => setSelectedMovie(null)}
+            />
+            <hr />
+            <h2>Similar movies</h2>
 
-  if (movies.length === 0) {
-    return <div>The list of movies is empty</div>;
-  }
+            {similarMovies.map((movie) => (
+            <MovieCard
+                key={movie.id}
+                movieData={movie}
+                onMovieClick={(newSelectedBook) => {
+                setSelectedMovie(newSelectedBook);
+                }}
+            />
+            ))}
+        </>
+        );
+    }
 
-  return (
-    <div>
-      {movies.map((movie) => (
-        <MovieCard
-          key={movie.id}
-          movieData={movie}
-          onMovieClick={(newSelectedBook) => {
-            setSelectedMovie(newSelectedBook);
-          }}
-        />
-      ))}
-    </div>
+    if (movies.length === 0) {
+        return <div>The list of movies is empty</div>;
+    }
+
+    return (
+        <div>
+        {movies.map((movie) => (
+            <MovieCard
+            key={movie.id}
+            movieData={movie}
+            onMovieClick={(newSelectedBook) => {
+                setSelectedMovie(newSelectedBook);
+            }}
+            />
+        ))}
+        </div>
   );
 };
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,27 +1,57 @@
 // MovieView receives property from the MainView - movieData
 // Property = result of function user click and giving value to selectedMovie variable
 // selectedMovie contains all info for MovieView component UI
+import PropTypes from 'prop-types';
 export const MovieView = ({ movieData, onBackClick }) => {
-    return (
+  return (
+    <div>
+      <img
+        src={movieData.image}
+        width='250'
+        alt={`Poster for ${movieData.title}`}
+      />
       <div>
-        <img src={movieData.image} width="250" />
-        <div>
-          <span>Title: </span>
-          <span>{movieData.title}</span>
-        </div>
-        <div>
-          <span>Description: </span>
-          <span>{movieData.description}</span>
-        </div>
-        <div>
-          <span>Director: </span>
-          <span>{movieData.director}</span>
-        </div>
-        <div>
-          <span>Genre: </span>
-          <span>{movieData.genre}</span>
-        </div>
-        <button onClick={() => {onBackClick()}}>Back</button>
+        <span>Title: </span>
+        <span>{movieData.title}</span>
       </div>
-    );
-  };
+      <div>
+        <span>Description: </span>
+        <span>{movieData.description}</span>
+      </div>
+      <div>
+        <span>Director: </span>
+        <span>{movieData.director.name}</span>
+      </div>
+      <div>
+        <span>Genre: </span>
+        <span>{movieData.genre.name}</span>
+      </div>
+      <button
+        onClick={() => {
+          onBackClick();
+        }}
+      >
+        Back
+      </button>
+    </div>
+  );
+};
+
+MovieView.propTypes = {
+  movieData: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    director: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        bio: PropTypes.string.isRequired,
+        birth: PropTypes.string.isRequired,
+        death: PropTypes.string,
+    }).isRequired,
+    genre: PropTypes.shape({
+        name: PropTypes.string.isRequired,
+        description: PropTypes.string.isRequired,
+    }).isRequired,
+    image: PropTypes.string.isRequired,
+  }).isRequired,
+  onBackClick: PropTypes.func.isRequired,
+};
